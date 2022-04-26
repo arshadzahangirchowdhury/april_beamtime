996f09d42dfeba1ed5aec0485b3bb4b406f064c7
M Arshad Zahangir Chowdhury <43049972+arshadzahangirchowdhury@users.noreply.github.com>
2022-04-26 04:01:10 -0400

    Update void_clustering.py

--- void_clustering.py
+++ void_clustering.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: M Arshad Zahangir Chowdhury
+Email: arshad.zahangir.bd[at]gmail[dot]com
+"""
+
 from sklearn import cluster
 from sklearn.cluster import KMeans, DBSCAN, OPTICS
 import numpy as np
@@ -24,15 +31,45 @@ import random
 def find_high_density_small_voids(features,estimators, keep_high_density_voids_only = True):
     '''
     Function to cluster all voids using their x,y,z center coordinates.
-    
     For DBSCAN, adjust eps based on elbow of reachability plot.
     For OPTICS, eps is auto-adjusted. 
     
-    args:
-    features: numpy array, x,y,z coordinates for each void center
-    estimators: list, each clustering algorithm is provided as (name, estimator class)
-    keep_high_density_voids_only: boolean, removes the voids which are large from the clustering plot. These are all points whom DBSCAN/OPTICS would label -1.
-    returns:
+    Parameters
+    ----------
+    features: numpy array, 
+        x,y,z coordinates for each void center
+    estimators: list, 
+        each clustering algorithm is provided as (name, estimator class)
+    keep_high_density_voids_only: boolean, 
+        removes the voids which are large from the clustering plot. These are all points whom DBSCAN/OPTICS would label -1.
+        
+        
+    example usage (uncomment the following and use it)
+
+    size = 1000
+    features = np.empty((size,3))
+    for idx in range(size):
+        features[idx][0]=random.randint(-100, 100)
+        features[idx][1]=random.randint(-100, 100)
+        features[idx][2]=random.randint(-100, 100)
+
+    print(features.shape)
+
+    df= pd.DataFrame()
+    df['x']=features[:,0]
+    df['y']=features[:,1]
+    df['z']=features[:,2]
+
+
+    find_high_density_small_voids(features, 
+                                  estimators = [
+        ("DBSCAN", DBSCAN(eps=8,min_samples=5)),
+        ("OPTICS", OPTICS(min_samples=10, cluster_method='xi')),
+    ],
+                                  keep_high_density_voids_only = True)
+    
+    Returns
+    ----------
     elbow plot for eps selection and clustering in plotly and saved figures
     
     '''
@@ -119,26 +156,3 @@ def find_high_density_small_voids(features,estimators, keep_high_density_voids_o
 
         fignum = fignum + 1
 
-# example usage (uncomment the following and use it)
-
-# size = 10000
-# features = np.empty((size,3))
-# for idx in range(size):
-#     features[idx][0]=random.randint(-100, 100)
-#     features[idx][1]=random.randint(-100, 100)
-#     features[idx][2]=random.randint(-100, 100)
-    
-# print(features.shape)
-
-# df= pd.DataFrame()
-# df['x']=features[:,0]
-# df['y']=features[:,1]
-# df['z']=features[:,2]
-
-
-find_high_density_small_voids(features, 
-                              estimators = [
-    ("DBSCAN", DBSCAN(eps=8,min_samples=5)),
-    ("OPTICS", OPTICS(min_samples=10, cluster_method='xi')),
-],
-                              keep_high_density_voids_only = True)
